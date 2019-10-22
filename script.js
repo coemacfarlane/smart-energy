bfc4faf4d2bee44706696ac10a604f67b4d98890
Coe M <madisonmacf@gmail.com>
2019-10-22 16:59:16 -0400

    working nav toggle

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function toggleOpen() {
-  document.getElementById("dropdownMenu").classList.toggle("show");
+  //   document.getElementById("dropdownMenu").classList.toggle("show");
+  var x = document.getElementById("dropdownMenu");
+  if (x.style.display == "none") {
+    x.style.display = "flex";
+  } else {
+    x.style.display = "none";
+  }
 }
