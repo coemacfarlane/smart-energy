2c4976370b765ff3cec563b27303d1f9ce270e03
Coe M <madisonmacf@gmail.com>
2019-10-23 11:53:19 -0400

    jquery simplified hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-function toggleOpen() {
-  //   document.getElementById("dropdownMenu").classList.toggle("show");
-  var x = document.getElementById("dropdownMenu");
-  if (x.style.display == "none") {
-    x.style.display = "flex";
-  } else {
-    x.style.display = "none";
-  }
-}
+$(document).ready(function() {
+  $(".hamburger").on("click", function() {
+    $(this).toggleClass("isActive");
+    $("#dropdownMenu").toggleClass("show");
+  });
+});
