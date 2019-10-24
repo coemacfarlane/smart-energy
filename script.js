1e661cdaf59d57734be54510c32fb6818e3e09c0
Coe M <madisonmacf@gmail.com>
2019-10-24 10:46:54 -0400

    credits page done, coal qa functional

--- script.js
+++ script.js
@@ -4,8 +4,25 @@ $(document).ready(function() {
     $("#dropdownMenu").toggleClass("show");
   });
 
-  $(".accordion").on("click", function() {
-    $(this).toggleClass("isActive");
-    $(".a").toggleClass("block");
-  });
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      var a = this.nextElementSibling;
+      if (a.style.display === "block") {
+        a.style.display = "none";
+      } else {
+        a.style.display = "block";
+      }
+    });
+  }
+  // $(".accordion").on("click", function() {
+  //   $(this).toggleClass("isActive");
+  //   $(".a").toggleClass("block");
 });
